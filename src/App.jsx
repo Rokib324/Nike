77c09d1d3b81b1f05d7941ad09c02732f0f0daa2
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-07-15 15:18:28 +0600

    all sections initiated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='relative '>
+      NAVIGATION
+      <section className='xl:padding-1 wide:padding-r padding-b'>Hero</section>
+      <section className='padding'>
+        PopularProducts
+      </section>
+      <section className='padding'>
+        SuperQuality
+      </section>
+      <section className='padding'>
+        Services
+      </section>
+      <section className='padding'>
+        SpecialOffer
+      </section>
+      <section className='bg-pale-blue padding'>
+        CustomerReviews
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        Subscribe
+      </section>
+      <section className='padding-x padding-t pb-8 bg-black'>
+        Footet
+      </section>
+    </main>
   )
 }
+
+export default App
